f1259d87ac580fc9ca315c1a6ed076927400bbaf
warrenshiv <warrenshiv@gmail.com>
2024-03-26 13:39:38 +0300

    Add t.ts file with imports

---
+++ src/t.ts
@@ -0,0 +1,15 @@
+import {
+    Canister,
+    query,
+    text,
+    update,
+    Record,
+    StableBTreeMap,
+    Vec,
+    Result,
+    nat64,
+    ic,
+    Opt,
+    int32,
+  } from "azle";
+  import { v4 as uuidv4 } from "uuid";
